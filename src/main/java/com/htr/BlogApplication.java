2c8e1e3e04a12e2b8de2df19823c46fc8fe53801
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-09-25 21:18:16 +0100

    added exception handling classes, ControllerExceptionHandler intercepts all exceptions, NotFoundException intercepts resource not found exception

--- src/main/java/com/htr/BlogApplication.java
+++ src/main/java/com/htr/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.htr.blog;
+package com.htr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/htr/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.htr.web;
+
+import com.htr.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+//        int i = 9/0;
+        String blog = null;
+        if (blog == null){
+            throw new NotFoundException("Blog does not exist");
+        }
+        return "index";
+    }
+}
