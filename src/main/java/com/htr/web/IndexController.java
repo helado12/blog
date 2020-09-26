5a4ecd36f831691ea9102edaab889af364c067c3
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-09-26 11:16:01 +0100

    1. logging, include request url, visitor's ip, classMethod, args, return content 2. added a aspect package, use Aop @PointCut

--- src/main/java/com/htr/web/IndexController.java
+++ src/main/java/com/htr/web/IndexController.java
@@ -3,17 +3,19 @@ package com.htr.web;
 import com.htr.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name){
 //        int i = 9/0;
-        String blog = null;
-        if (blog == null){
-            throw new NotFoundException("Blog does not exist");
-        }
+//        String blog = null;
+//        if (blog == null){
+//            throw new NotFoundException("Blog does not exist");
+//        }
+
         return "index";
     }
 }
