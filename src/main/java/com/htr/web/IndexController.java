c168085c814d449b079927b69ad69502b2d2756b
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-09-30 22:16:04 +0100

    Note problem: not albe to get static resources is because the url for static resources such as http://localhost:8080/css/me.css gets mapped to a method in IndexController.java and it has two parameters. The method requires first parameter to be an integer but "css" is a string so get the error that it cannot convert a string to an integer in the parameter. First do not add the mapping to solve the problem

--- src/main/java/com/htr/web/IndexController.java
+++ src/main/java/com/htr/web/IndexController.java
@@ -8,16 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
-//        int i = 9/0;
-//        String blog = null;
-//        if (blog == null){
-//            throw new NotFoundException("Blog does not exist");
-//        }
-
-        return "index";
-    }
+//    @GetMapping("/{id}/{name}")
+//    public String index(@PathVariable Integer id, @PathVariable String name){
+//
+//        return "index";
+//    }
 
     @GetMapping("/blog")
     public String blog(){
